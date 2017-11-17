f371711b3ab99354a42ba9c00bb302dd34e77ef9
Matt Thompson <mattthompson@Matts-MacBook-Pro.local>
2017-11-17 11:04:57 -0600

    test

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ spirit.setup().then(() => { // load from CDN
   // your code here..
   // for example spirit.create({...})  
   
-  spirit.load('../json/animation-one.json').then(groups => {
+  spirit.load('./json/animation-one.json').then(groups => {
     groups.at(0).construct().play() // play first group
   })
   
